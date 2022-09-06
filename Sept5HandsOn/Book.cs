9038ffd79c9610904afba20b9651dd6d2696980c
Rakshitha MP <75797183+Rakshithamp47@users.noreply.github.com>
2022-09-06 14:30:27 +0530

    Add files via upload

---
+++ Sept5HandsOn/Book.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HRLibrary
+{
+	public class Book
+	{
+		public Book()
+		{
+
+		}
+
+		public Book(int id,string name,int price,string author)
+		{
+			_bookid = id;
+			_bookname = name;
+			_price = price;
+			_author = author;
+		}
+
+		
+		private int _bookid;
+
+		public int BookID
+		{
+			get { return _bookid; }
+			set { _bookid = value; }
+		}
+
+		private string _bookname;
+
+		public string BookName
+		{
+			get { return _bookname; }
+			set { _bookname = value; }
+		}
+		private int _price;
+
+		public int BookPrice
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+
+		private string _author;
+
+		public string BookAuthor
+		{
+			get { return _author; }
+			set { _author = value; }
+		}
+
+
+        public void InsertBook(int BookID, string BooKName)
+        {
+            this.BookID = BookID;
+            this.BookName = BooKName;
+
+        }
+
+
+
+        public void InsertBook(int BookID, string BooKName, int Price)
+        {
+            this.BookID = BookID;
+            this.BookName = BooKName;
+            this.BookPrice = Price;
+
+        }
+
+        public void InsertBook(int BookID, string BooKName, int Price, string author )
+        {
+            this.BookID = BookID;
+			this.BookName = BooKName;
+			this.BookPrice = Price;
+			this.BookAuthor = author;
+
+        }
+
+        public class Novel : Book
+        {
+            int NoOfPages;
+            public void InsertBook(int id, string name, int price, int noofpages)
+            {
+                this.BookID = id;
+                this.BookName = name;
+                this.BookPrice = price;
+                this.NoOfPages = noofpages;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Sept5HandsOn/BookCategory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class BookCategory
+    {
+
+		public BookCategory()
+		{
+
+		}
+
+
+        public BookCategory(int id, string name, string des)
+        {
+			_catid = id;
+			_catname = name;
+			_description = des;
+
+        }
+
+
+
+
+
+
+        private int _catid;
+
+		public int CategoryId
+		{
+			get { return _catid; }
+			set { _catid = value; }
+		}
+
+		private string _catname;
+
+		public string CategoryName
+		{
+			get { return _catname; }
+			set { _catname = value; }
+		}
+
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+
+        
+
+
+    }
+}
