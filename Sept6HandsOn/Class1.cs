1eeacb6d20ead2dcde1717dd2372ac9c482ba8cf
Rakshitha MP <75797183+Rakshithamp47@users.noreply.github.com>
2022-09-07 07:20:36 +0530

    Add files via upload

---
+++ Sept6HandsOn/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public interface Ibank2
+    {
+
+        void print_balance();
+        void calc_interest();
+    }
+    public class Saving : Ibank2
+    {
+
+        public double amount;
+
+        public Saving()
+        {
+
+            amount = 0;
+        }
+
+        public void print_balance()
+        {
+            Console.WriteLine("ENTER THE AMOUNT");
+            amount = Convert.ToDouble(Console.ReadLine());
+        }
+        public void calc_interest()
+        {
+            float r = 0.04f;
+
+            double result = amount * r;
+            Console.WriteLine("calc_interest " + result);
+        }
+    }
+    public class Current : Ibank2
+    {
+
+        public double amount;
+
+        public Current()
+        {
+
+            amount = 0;
+        }
+
+        public void print_balance()
+        {
+            Console.WriteLine("ENTER THE AMOUNT");
+            amount = Convert.ToDouble(Console.ReadLine());
+        }
+        public void calc_interest()
+        {
+            float r = 0.02f;
+
+            double result = amount * r;
+            Console.WriteLine("calc_interest " + result);
+        }
+    }
+}
---
+++ Sept6HandsOn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+namespace Sept6Handson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p;
+            p = new Employee();
+            p.work();
+            p = new Manager();
+            p.work();
+            p = new Clerk();
+            p.work();    //1
+
+            Bank b1 = new Bank();
+            b1.print_balance();
+            b1.calc_interest();     //2
+
+            Console.WriteLine("savings account");
+            Ibank2 b = new Saving();
+            b.print_balance();
+            b.calc_interest();
+            Console.WriteLine("current aacount");
+            Ibank2 b2 = new Current();
+            b2.print_balance();
+            b2.calc_interest();   //3
+        }
+    }
+}
